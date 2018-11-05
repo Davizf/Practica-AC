f1aeedeb3db3334b8c5923bdea0ebd3989417edc
carlosgarciacanibano <garciacanibanocarlos@gmail.com>
2018-11-05 16:23:04 +0100

    SpaceObject v1

---
+++ spaceObject v1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct SpaceObject {
+	boolean isAsteroid;
+	//X axis position
+	auto x;
+	//Y axis position
+	auto y;
+	//X axis speed
+	auto vx;
+	//Y axis speed
+	auto vy;
+	//Object mass
+	auto m;
+};
