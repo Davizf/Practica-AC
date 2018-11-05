4b174259629498f89931117a43d2be708bf983de
Daviz <37458551+Davizf@users.noreply.github.com>
2018-11-05 18:46:00 +0100

    Update nasteroids-seq.cpp

--- nasteroids-seq.cpp
+++ nasteroids-seq.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int num_asteroides;
-int num_iteraciones;
-int num_planetas;
-int semilla;
+int num_asteroids;
+int num_iteracions;
+int num_planets;
+int seed;
 
 int main() {
   cout << "Enter the number of asteroids:";
-  cin >> num_asteroides;
+  cin >> num_asteroids;
   cout << "Enter the number of iterations:";
-  cin >> num_iteraciones;
+  cin >> num_iteracions;
   cout << "Enter the number of planets:";
-  cin >> num_planetas;
+  cin >> num_planets;
   cout << "Enter the seed number:";
-  cin >> semilla;
+  cin >> seed;
 
-  if(num_asteroides<0 || num_iteraciones<0 || num_planetas<0 || semilla<=0){
-    cout << "nasteroids-seq: Wrong arguments.\nCorrect use:\nnasteroid-seq num_asteroides num_iteraciones num_planetas semilla\n";
+  if(num_asteroids<0 || num_iteracions<0 || num_planets<0 || seed<=0){
+    cout << "nasteroids-seq: Wrong arguments.\nCorrect use:\nnasteroid-seq num_asteroids num_iteracions num_planets seed\n";
     return -1;
   }
   return 0;
