2ec21b7f915604ca5f285b4f8e02de048c651d7d
carlosgarciacanibano <garciacanibanocarlos@gmail.com>
2018-11-07 20:25:14 +0100

    Añadidas funcionalidades
    Añadidas las distribuciones aleatorias, la creación de asteroides y la referencia a la interfaz de clase SpaceObject.

--- nasteroids-seq.cpp
+++ nasteroids-seq.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <random>
+#include "SpaceObject.h"
 using namespace std;
 
 int num_asteroids;
 int num_iteracions;
 int num_planets;
 int seed;
+int width = 200;
+int height = 200;
+double mass = 1000;
+double sdm = 50;
+vector<SpaceObject> asteroids;
+vector<SpaceObject> planets;
 
 int main() {
   cout << "Enter the number of asteroids:";
@@ -20,5 +28,20 @@ int main() {
     cout << "nasteroids-seq: Wrong arguments.\nCorrect use:\nnasteroid-seq num_asteroids num_iteracions num_planets seed\n";
     return -1;
   }
+  
+  //Random distributions
+  default_random_engine re{semilla};
+  uniform_real_distribution<double> xdist{0.0, std::nextafter(width,
+                                        std::numeric_limits<double>::max())};
+  uniform_real_distribution<double> ydist{0.0, std::nextafter(height,
+                                        std::numeric_limits<double>::max())};
+  normal_distribution<double> mdist{mass, sdm};
+  
+  //Creating asteroids...
+  for(int i = 0; i < num_asteroides; i++){
+    SpaceObject anyKind(xdist(re), ydist(re), 0.0, 0.0, mdist(re));
+    asteroids[i] = anyKind;
+  }
+
   return 0;
 }
