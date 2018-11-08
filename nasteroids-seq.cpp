0a81c8fbda1319ef67570ce604b5b0d0d3fa18b8
Daviz <37458551+Davizf@users.noreply.github.com>
2018-11-08 01:47:28 +0100

    Correccion de la generacion de planetas

--- nasteroids-seq.cpp
+++ nasteroids-seq.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int num_asteroids;
-int num_iteracions;
+int num_iterations;
 int num_planets;
 int seed;
 int width = 200;
@@ -15,46 +15,49 @@ vector<SpaceObject> asteroids;
 vector<SpaceObject> planets;
 
 int main() {
+  //Input
   cout << "Enter the number of asteroids:";
   cin >> num_asteroids;
-  cout << "Enter the number of iterations:";
-  cin >> num_iteracions;
-  cout << "Enter the number of planets:";
+  cerr << "Enter the number of iterations:";
+  cin >> num_iterations;
+  cerr << "Enter the number of planets:";
   cin >> num_planets;
-  cout << "Enter the seed number:";
+  cerr << "Enter the seed number:";
   cin >> seed;
 
-  if(num_asteroids<0 || num_iteracions<0 || num_planets<0 || seed<=0){
-    cout << "nasteroids-seq: Wrong arguments.\nCorrect use:\nnasteroid-seq num_asteroids num_iteracions num_planets seed\n";
+  //Check input conditions
+  if(num_asteroids<0 || num_iterations<0 || num_planets<0 || seed<=0){
+    cout << "nasteroids-seq: Wrong arguments.\nCorrect use:\nnasteroid-seq num_asteroids num_iterations num_planets seed\n";
     return -1;
   }
-  
+
   //Random distributions
-  default_random_engine re{semilla};
+  default_random_engine re{seed};
   uniform_real_distribution<double> xdist{0.0, std::nextafter(width,
                                         std::numeric_limits<double>::max())};
   uniform_real_distribution<double> ydist{0.0, std::nextafter(height,
                                         std::numeric_limits<double>::max())};
   normal_distribution<double> mdist{mass, sdm};
-  
+
   //Creating asteroids...
   for(int i = 0; i < num_asteroids; i++){
     SpaceObject anyKind(xdist(re), ydist(re), 0.0, 0.0, mdist(re));
     anyKind.isAsteroid = true;
     asteroids[i] = anyKind;
   }
-  
+
   //Creating planets...
   for(int i = 0; i < num_planets; i++){
-    //Clockwise planet placement... 
+    SpaceObject anyKind(0.0,0.0,0.0,0.0,0.0);
+    //Clockwise planet placement...
     if(i % 4 == 0){
-      SpaceObject anyKind(xdist(re), 0.0, 0.0, 0.0, mdist(re) * 10);
-    } else if(i % 4 == 1) {
-      SpaceObject anyKind(width, ydist(re), 0.0, 0.0, mdist(re) * 10);
-    } else if(i % 4 == 2){
       SpaceObject anyKind(xdist(re), height, 0.0, 0.0, mdist(re) * 10);
-    } else if(i % 4 == 3){
+    } else if(i % 4 == 1) {
       SpaceObject anyKind(0, ydist(re), 0.0, 0.0, mdist(re) * 10);
+    } else if(i % 4 == 2){
+      SpaceObject anyKind(xdist(re), 0, 0.0, 0.0, mdist(re) * 10);
+    } else if(i % 4 == 3){
+      SpaceObject anyKind(width, ydist(re), 0.0, 0.0, mdist(re) * 10);
     }
     anyKind.isAsteroid = false;
     planets[i] = anyKind;
