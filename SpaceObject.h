31fadbf0021e158bcd8973c192393e59717e9318
juanjo698 <44554915+juanjo698@users.noreply.github.com>
2018-11-08 16:27:08 +0100

    Add files via upload

---
+++ SpaceObject.h
@@ -0,0 +1,28 @@
+
+#ifndef SpaceObject_h
+#define SpaceObject_h
+
+class SpaceObject{
+public:
+	bool isAsteroid;
+	//X axis position
+	double x;
+	//Y axis position
+	double y;
+	//X axis speed
+	double vx;
+	//Y axis speed
+	double vy;
+	//Object mass
+	double m;
+	//Default constructor
+	SpaceObject(double, double, double, double, double);
+};
+SpaceObject::SpaceObject(double x1, double y1, double vx1, double vy1, double m1){
+	x = x1;
+	y = y1;
+	vx = vx1;
+	vy = vy1;
+	m = m1;
+}
+#endif
