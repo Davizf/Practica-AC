1002496cff22d421a3caabb11a6d59b97e0b9ac1
Daviz <37458551+Davizf@users.noreply.github.com>
2018-11-10 00:17:15 +0100

    Solucionado el problema de la distancia^2

--- math_functionsv1.cpp
+++ math_functionsv1.cpp
@@ -50,8 +50,8 @@ vector<double> normal_movement(SpaceObject n1, SpaceObject n2) {
 	cout << "angle " << angle << "\n";
 	cout << "distance " << distance_between_n1_n2 << "\n";
 	//Second, we have to calculate forces
-	n1_x_axis_force = ((gravity * n1.m * n2.m) / (distance_between_n1_n2)) * cos(angle);
-	n1_y_axis_force = ((gravity * n1.m * n2.m) / (distance_between_n1_n2)) * sin(angle);
+	n1_x_axis_force = ((gravity * n1.m * n2.m) / pow(distance_between_n1_n2,2)) * cos(angle);
+	n1_y_axis_force = ((gravity * n1.m * n2.m) / pow(distance_between_n1_n2,2)) * sin(angle);
 
 	if (n1_x_axis_force > 200) n1_x_axis_force = 200;
 	if (n1_y_axis_force > 200) n1_y_axis_force = 200;
