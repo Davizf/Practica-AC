dd2524f0d9965eb9b2fddc4cd11f74b34a3d3ef0
juanjo698 <44554915+juanjo698@users.noreply.github.com>
2018-11-16 11:42:12 +0100

    Mejoras implementadas y codigo limpio

---
+++ SpaceObject.h
@@ -0,0 +1,58 @@
+#ifndef SpaceObject_h
+#define SpaceObject_h
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class SpaceObject{
+public:
+	bool isAsteroid;
+	//X axis position
+	double x;
+	//Y axis position
+	double y;
+	//X axis speed
+	double vx;
+	//Y axis speed
+	double vy;
+	//Before collision X axis speed
+	double before_vx;
+	//Before collision Y axis speed
+	double before_vy;
+	//Before change x position
+	double before_x;
+	//Before change y position
+	double before_y;
+	//Object mass
+	double m;
+	//All collisions
+	int collisions;
+	//Default constructor
+	SpaceObject(double, double, double, double, double, int);
+	SpaceObject(double, double, double, double, double);
+};
+SpaceObject::SpaceObject(double x1, double y1, double vx1, double vy1, double m1, int num_asteroids){
+	x = x1;
+	y = y1;
+	vx = vx1;
+	vy = vy1;
+	m = m1;
+	//Collision counter
+	collisions = 0;
+	before_vx = 0;
+	before_vy = 0;
+	before_x = 0;
+	before_y = 0;
+}
+SpaceObject::SpaceObject(double x1, double y1, double vx1, double vy1, double m1){
+	x = x1;
+	y = y1;
+	vx = vx1;
+	vy = vy1;
+	m = m1;
+	before_vx = 0;
+	before_vy = 0;
+	before_x = 0;
+	before_y = 0;
+}
+#endif
